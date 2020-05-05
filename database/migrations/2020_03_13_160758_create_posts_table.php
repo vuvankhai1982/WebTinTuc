e7452425ffc25c4fcb316f2799cbbb6742f29e56
Khai.Hoan <khaihoan250382@gmail.com>
2020-05-05 16:23:57 +0700

    index

---
+++ database/migrations/2020_03_13_160758_create_posts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedTinyInteger('status_id');
+            $table->unsignedTinyInteger('type_id');
+            $table->unsignedTinyInteger('noi bat');
+            $table->unsignedBigInteger('user_id');
+            $table->string('image')->nullable();
+            $table->string('image_url')->nullable();
+            $table->string('thumbnail_image')->nullable();
+            $table->text('tom tat')->nullable();
+            $table->text('noi dung')->nullable();
+
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
